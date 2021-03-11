c6e92cf3d18fbf6300ec9d11c7b2bef4343d2fcb
Curo <33403861+Curo-Dev@users.noreply.github.com>
2021-03-11 14:16:29 +0900

    feat: 아무 레이어도 선택되지 않으면 빈 Null Object 생성

--- Automatic Null Object.jsx
+++ Automatic Null Object.jsx
@@ -1,11 +1,11 @@
 var proj = app.project;
 if (proj) {
-    var CompItem = app.project.activeItem;
-    if (CompItem != null && (CompItem instanceof CompItem)) {
+    var comp = app.project.activeItem;
+    if (comp != null && (comp instanceof CompItem)) {
         app.beginUndoGroup("");
-        var selectLayers = CompItem.selectedLayers;
+        var selectLayers = comp.selectedLayers;
         if (selectLayers.length >= 1) {
-            var newNull = CompItem.layers.addNull(CompItem.duration);
+            var newNull = comp.layers.addNull(comp.duration);
             for (i = 0; i <= selectLayers.length - 1; i++) {
                 currentLayer = selectLayers[i];
                 newNull.moveBefore(selectLayers[0]);
@@ -13,7 +13,7 @@ if (proj) {
                 app.endUndoGroup();
             }
         } else {
-            alert("레이어를 선택해 주세요");
+            comp.layers.addNull(comp.duration);
         }
     } else {
         alert("컴포지션를 선택해 주세요");
